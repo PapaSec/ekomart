8bc3b170167d10a953bb56999b9ae549575fb443
PapaSec <fransronald48@gmail.com>
2026-07-26 22:22:48 +0200

    Daily commit

--- database/migrations/2026_07_26_153538_create_categories_table.php
+++ database/migrations/2026_07_26_153538_create_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,13 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('icon')->nullable();     
+            $table->string('image')->nullable();    
+            $table->boolean('is_featured')->default(false);
+            $table->boolean('is_active')->default(true);
+            $table->integer('sort_order')->default(0);
             $table->timestamps();
         });
     }
