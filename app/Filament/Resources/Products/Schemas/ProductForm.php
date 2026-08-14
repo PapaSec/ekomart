bc4758d572d5855ec8d2246095a3a78c00d4af1c
PapaSec <fransronald48@gmail.com>
2026-08-14 18:28:20 +0200

    Filament products

--- app/Filament/Resources/Products/Schemas/ProductForm.php
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -3,14 +3,10 @@
 namespace App\Filament\Resources\Products\Schemas;
 
 use Illuminate\Support\Str;
+
+use Filament\Forms\Components\{FileUpload, MarkdownEditor, Select, TextInput, Toggle};
 use Filament\Schemas\Schema;
-use Filament\Schemas\Components\Grid;
-use Filament\Schemas\Components\Section;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\{Grid, Section};
 
 class ProductForm
 {
